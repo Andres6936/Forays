4ca04c8804f6a151ea87c31ab92eb526c4f0aba5
Joan Andrés <andres6936@live.com>
2020-12-17 22:41:56 -0500

    Implemented the read of save file

--- Forays/Loader/BinaryLoader.cs
+++ Forays/Loader/BinaryLoader.cs
@@ -2,12 +2,23 @@ using System.IO;
 
 namespace Forays.Loader
 {
-    public class BinaryLoader : Loader
+    public sealed class BinaryLoader : Loader
     {
         public BinaryLoader(string filename)
         {
             using (var reader = new BinaryReader(File.Open(filename, FileMode.Open)))
             {
+                NamePlayer = reader.ReadString();
+                CurrentLevelId = reader.ReadInt32();
+
+                var numberLevelTypes = reader.ReadInt32();
+                for (var index = 0; index < numberLevelTypes; index += 1)
+                {
+                    LevelTypes.Add((LevelType) reader.ReadInt32());
+                }
+
+                WizLite = reader.ReadBoolean();
+                WizDark = reader.ReadBoolean();
             }
         }
     }
