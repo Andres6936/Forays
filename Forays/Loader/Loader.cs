3059effeb0fcc37bd463dd3f96d509e2d511a290
Joan Andrés <andres6936@live.com>
2020-12-18 10:17:48 -0500

    The properties should be public

--- Forays/Loader/Loader.cs
+++ Forays/Loader/Loader.cs
@@ -5,32 +5,32 @@ namespace Forays.Loader
 {
     public abstract class Loader
     {
-        protected int CurrentLevelId { get; set; }
+        public int CurrentLevelId { get; set; }
 
-        protected int FinalLevelClock { get; set; }
+        public int FinalLevelClock { get; set; }
 
-        protected int FinalLevelDemonCount { get; set; }
+        public int FinalLevelDemonCount { get; set; }
 
-        protected int[] FinalLevelCultistCount { get; set; }
+        public int[] FinalLevelCultistCount { get; set; }
 
-        protected bool WizLite { get; set; }
+        public bool WizLite { get; set; }
 
-        protected bool WizDark { get; set; }
+        public bool WizDark { get; set; }
 
-        protected char[,] LastCharacterSeen { get; set; }
+        public char[,] LastCharacterSeen { get; set; }
 
-        protected string NamePlayer { get; set; }
+        public string NamePlayer { get; set; }
 
-        protected List<Actor> Tiebreakers { get; set; }
+        public List<Actor> Tiebreakers { get; set; }
 
-        protected List<FeatType> FeatTypes { get; set; }
+        public List<FeatType> FeatTypes { get; set; }
 
-        protected List<SpellType> SpellTypes { get; set; }
+        public List<SpellType> SpellTypes { get; set; }
 
-        protected List<LevelType> LevelTypes { get; set; }
+        public List<LevelType> LevelTypes { get; set; }
 
-        protected Color[,] LastColorCharacterSeen { get; set; }
+        public Color[,] LastColorCharacterSeen { get; set; }
 
-        protected Color[,] LastBackgroundColorCharacterSeen { get; set; }
+        public Color[,] LastBackgroundColorCharacterSeen { get; set; }
     }
 }
