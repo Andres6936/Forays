eb1690b9b6263109b4c8bc4599d874bcc4db6353
Joan Andrés <andres6936@live.com>
2020-12-17 22:22:15 -0500

    Added Variables Members

--- Forays/Loader/Loader.cs
+++ Forays/Loader/Loader.cs
@@ -1,6 +1,35 @@
+using System.Collections.Generic;
+
 namespace Forays.Loader
 {
     public abstract class Loader
     {
+        // Members
+
+        protected int CurrentLevelId;
+
+        protected int FinalLevelClock;
+
+        protected int FinalLevelDemonCount;
+
+        protected int[] FinalLevelCultistCount;
+
+        protected bool WizLite;
+
+        protected bool WizDark;
+
+        protected char[,] LastCharacterSeen;
+
+        protected string NamePlayer;
+
+        protected List<LevelType> LevelType;
+
+        protected Color[,] LastColorCharacterSeen;
+
+        protected Color[,] LastBackgroundColorCharacterSeen;
+
+        // Getters Abstract
+
+        public abstract string GetNamePlayer();
     }
 }
