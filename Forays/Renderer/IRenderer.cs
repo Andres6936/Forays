c88e9fab87ebbee8eb1c6a7fde9b5c7233e09bb5
Joan Andrés <andres6936@live.com>
2021-01-15 15:36:42 -0500

    Added documentation

--- Forays/Renderer/IRenderer.cs
+++ Forays/Renderer/IRenderer.cs
@@ -3,6 +3,11 @@ using Forays.Scenes;
 
 namespace Forays.Renderer
 {
+    /// <summary>
+    /// The IRenderer has the responsibility of define the method that the
+    /// engines must be follow to allow drawn in the window and that the user
+    /// can visualize objects, text, figures, images, etc ....  
+    /// </summary>
     public interface IRenderer
     {
         bool IsRunning();
