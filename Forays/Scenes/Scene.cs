fe225119b39548e3828202134e98a0f058e78f5c
Joan Andrés <andres6936@live.com>
2021-01-15 13:41:49 -0500

    Added documentation

--- Forays/Scenes/Scene.cs
+++ Forays/Scenes/Scene.cs
@@ -8,7 +8,10 @@ namespace Forays.Scenes
 
         /// <summary>
         /// Current instance of renderer engine, current the application support
-        /// two renderers: OpenTk (OpenGL) and Console (Terminal). 
+        /// two renderers: OpenTk (OpenGL) and Console (Terminal).
+        /// 
+        /// The first scene instantiated has the responsibility of initialize
+        /// the renderer.  
         /// </summary>
         private IRenderer renderer;
 
