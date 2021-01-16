04831b5c7a990fb863e5aa67a55f017bc415b020
Joan Andrés <andres6936@live.com>
2021-01-16 12:10:27 -0500

    Deleted unused code DefineSpriteAcross

--- Forays/Graphics/SpriteType.cs
+++ Forays/Graphics/SpriteType.cs
@@ -52,27 +52,6 @@ namespace Forays
 
         public static SpriteType DefineSpriteAcross(Surface surface, int sprite_width_px,
             int sprite_height_px,
-            int num_columns)
-        {
-            SpriteType s = new SpriteType();
-            float texcoord_width =
-                (float) sprite_width_px * 1.0f / (float) surface.Texture.TextureWidthPx;
-            float texcoord_height = (float) sprite_height_px * 1.0f /
-                                    (float) surface.Texture.TextureHeightPx;
-            s.X = idx => (idx % num_columns) * texcoord_width;
-            s.Y = idx => (idx / num_columns) * texcoord_height;
-            s.SpriteWidth = texcoord_width;
-            s.SpriteHeight = texcoord_height;
-            if (surface != null)
-            {
-                surface.Texture.Sprite.Add(s);
-            }
-
-            return s;
-        }
-
-        public static SpriteType DefineSpriteAcross(Surface surface, int sprite_width_px,
-            int sprite_height_px,
             int num_columns, int h_offset_px, int v_offset_px)
         {
             SpriteType s = new SpriteType();
