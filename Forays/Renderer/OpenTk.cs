2d5aa2d1a26aa27a711e58d52e77afd14a3bd6ea
Joan Andrés <andres6936@live.com>
2021-01-16 13:08:34 -0500

    Deleted comments

--- Forays/Renderer/OpenTk.cs
+++ Forays/Renderer/OpenTk.cs
@@ -149,21 +149,6 @@ namespace Forays.Renderer
                 true; //todo: changed this to simply set Resizing instead of actually calling resizing methods.
         }
 
-
-        /*public static void ReplaceTexture(int texture_unit,string filename){ //binds a texture to the given texture unit, replacing the texture that's already there
-            if(String.IsNullOrEmpty(filename)){
-                throw new ArgumentException(filename);
-            }
-            GL.ActiveTexture(TextureUnit.Texture0 + texture_unit);
-            int id = GL.GenTexture();
-            GL.BindTexture(TextureTarget.Texture2D,id);
-            Bitmap bmp = new Bitmap(filename);
-            BitmapData bmp_data = bmp.LockBits(new Rectangle(0,0,bmp.Width,bmp.Height),ImageLockMode.ReadOnly,System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            GL.TexImage2D(TextureTarget.Texture2D,0,PixelInternalFormat.Rgba,bmp_data.Width,bmp_data.Height,0,OpenTK.Graphics.OpenGL.PixelFormat.Bgra,PixelType.UnsignedByte,bmp_data.Scan0);
-            bmp.UnlockBits(bmp_data);
-            GL.TexParameter(TextureTarget.Texture2D,TextureParameterName.TextureMinFilter,(int)TextureMinFilter.Nearest);
-            GL.TexParameter(TextureTarget.Texture2D,TextureParameterName.TextureMagFilter,(int)TextureMagFilter.Nearest);
-        }*/
         public void UpdatePositionVertexArray(Surface s, IList<int> index_list,
             IList<int> layout_list = null)
         {
