b521fa7ea31eab1fef7091dd7d94f14c2c2779f8
Joan Andrés <andres6936@live.com>
2021-01-16 07:55:08 -0500

    Deleted unused method CreateISO

--- Forays/Graphics/CellLayout.cs
+++ Forays/Graphics/CellLayout.cs
@@ -45,36 +45,6 @@ namespace Forays
             }
         }
 
-        public static CellLayout CreateIso(Surface s, int rows, int cols, int cell_height_px,
-            int cell_width_px,
-            int v_offset_px, int h_offset_px, int cell_v_offset_px, int cell_h_offset_px,
-            PositionFromIndex z = null,
-            PositionFromIndex elevation = null)
-        {
-            CellLayout c = new CellLayout();
-            c.CellHeightPx = cell_height_px;
-            c.CellWidthPx = cell_width_px;
-            c.VerticalOffsetPx = v_offset_px;
-            c.HorizontalOffsetPx = h_offset_px;
-            c.X = idx => (rows - 1 - (idx / cols) + (idx % cols)) * cell_h_offset_px;
-            if (elevation == null)
-            {
-                c.Y = idx => ((idx / cols) + (idx % cols)) * cell_v_offset_px;
-            }
-            else
-            {
-                c.Y = idx => ((idx / cols) + (idx % cols)) * cell_v_offset_px + elevation(idx);
-            }
-
-            c.Z = z;
-            if (s != null)
-            {
-                s.layouts.Add(c);
-            }
-
-            return c;
-        }
-
         public static CellLayout CreateIsoAtOffset(Surface s, int rows, int cols,
             int base_start_row,
             int base_start_col, int base_rows, int cell_height_px, int cell_width_px,
