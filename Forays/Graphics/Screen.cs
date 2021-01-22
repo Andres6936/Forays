035c735d729c56d8cfde964f4f973cc6422db793
Joan Andrés <andres6936@live.com>
2021-01-22 12:13:02 -0500

    Deleted macro code

--- Forays/Graphics/Screen.cs
+++ Forays/Graphics/Screen.cs
@@ -117,10 +117,6 @@ namespace Forays
         /// </summary>
         static Screen()
         {
-#if CONSOLE
-            Screen.GLMode = false;
-#endif
-
             memory = new ColorChar[Global.SCREEN_H, Global.SCREEN_W];
             for (int i = 0; i < Global.SCREEN_H; ++i)
             {
